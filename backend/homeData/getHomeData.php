ca895bcd148cc86436a439100235353916a6ab54
OmarTegar <omarahmed_3@yahoo.com>
2026-09-13 05:11:41 +0300

    ORDER BY posts DESC

--- backend/homeData/getHomeData.php
+++ backend/homeData/getHomeData.php
@@ -11,7 +11,7 @@ try {
     require(__DIR__ . '/../config/conn.php');
 
     $friendsStmt = $pdo->prepare("SELECT id, first_name, last_name, email FROM users");
-    $postStmt = $pdo->prepare("SELECT id, user_id, content, image, created_at FROM posts");
+    $postStmt = $pdo->prepare("SELECT id, user_id, content, image, created_at FROM posts ORDER BY id DESC");
 
     if (!$friendsStmt->execute()) {
         echo json_encode(['status' => "failed", 'message' => "An error has occurred while returning friends list"]);
