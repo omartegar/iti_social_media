a89d887aa53a2abf49da3b78d0f0f41a3d452830
OmarTegar <omarahmed_3@yahoo.com>
2026-09-14 01:01:18 +0300

    getPrivateMessages fix

--- backend/messages/getPrivateMessages.php
+++ backend/messages/getPrivateMessages.php
@@ -17,9 +17,9 @@ try {
     OR receiver_id = :sender_id
     AND sender_id = :receiver_id
     INNER JOIN users
+    ON users.id = messages.sender_id
+    OR users.id = messages.receiver_id
     ORDER BY id DESC");
-    // ON users.id = messages.sender_id
-    // OR users.id = messages.receiver_id
 
     $receiver_data = $pdo->prepare("SELECT id, first_name, last_name, email, created_at
     FROM users WHERE id = :id");
